e54ea9f9260bf9b4f9885e8e0f035f9ecedacd2b
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-04-16 22:35:18 -0400

    Update secret ptah

--- data.tf
+++ data.tf
@@ -1,7 +1,3 @@
-data "aws_ssm_parameter" "cloudflare_zone_id" {
-  name = "/homelab/cloudflare_zone_id"
-}
-
 data "vault_generic_secret" "cloudflare" {
-  path = "secret/data/cloudflare"
+  path = "secret/cloudflare"
 }
